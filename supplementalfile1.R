31821bc4445a2e42da85032e5ae7775f46ccff8a
Nathan <nathan.wally.anderson@gmail.com>
2020-07-20 21:06:47 -0500

    created new function

--- supplementalfile1.R
+++ supplementalfile1.R
@@ -10,80 +10,39 @@
 #      should be 0.5 unless other data is avaialable
 
 # function for P(SA)
-Pfsa <- function(Da, scs, mud = 0.5){
+Pfsa <- function(Da, scs, mud = 0.5){ #(Da, Xs, Y, Dd, Ds, mud = 0.5){
   sexchroms <- strsplit(scs, split = "")[[1]]
-  if(scs %in% c("XO", "XXO", "XY", "XYY", "XXY", "XYYY", "XXXY")){
+  if("X" %in% sexchroms){
     Xs <- sum(sexchroms == "X")
     Y <- sum(sexchroms == "Y")
     Ds <- Da + Xs + Y
     Dd <- Da + 2 * Xs
-    res <- 1 - mud * ((Da * (Da - 2) + 2 * Xs * (2 * Xs - 2)) / (Dd * (Dd - 2))) -
-      (1 - mud) * ((Da * (Da - 2) + max(c(Xs, Y)) * (max(c(Xs, Y)) - 1)) / (Ds * (Ds - 2)))
+    res <- 1 - mud * ((Da * (Da - 2) + 4 * Xs * (Xs - 1)) / (Dd * (Dd - 2))) - 
+      (1 - mud) * (((Xs * (Xs - 1))/(Ds * (Da + Xs - 1))) + 
+                     ((Y * (Y - 1))/(Ds * (Da + Y - 1))) + 
+                     ((Da * (Da - 2))/(Ds * (Ds - 2))))
   }
-  if(scs %in% c("ZO", "ZZO", "ZW", "ZWW", "ZZW", "ZWWW", "ZZZW")){
+  if("Z" %in% sexchroms){
     Zd <- sum(sexchroms == "Z")
     W <- sum(sexchroms == "W")
     Dd <- Da + Zd + W
     Ds <- Da + 2 * Zd
     mus <- 1-mud
-    res <- 1 - mus * ((Da * (Da - 2) + 2 * Zd * (2 * Zd - 2)) / (2 * Ds * (Ds - 2))) -
-      (1 - mus) * ((Da * (Da - 2) + max(c(Zd, W)) * (max(c(Zd, W)) - 1)) / (2 * Dd * (Dd - 2)))
+    
+    res <- 1 - mus * ((Da * (Da - 2) + 4 * Zd * (Zd - 1)) / (Ds * (Ds - 2))) - 
+      (1 - mus) * (((Zd * (Zd - 1))/(Dd * (Da + Zd - 1))) + 
+                     ((W * (W - 1))/(Dd * (Da + W - 1))) + 
+                     ((Da * (Da - 2))/(Dd * (Dd - 2))))
   }
+  
   return(res)
 }
+
+
 # function for P(AA)
-Pfaa <- function(Da, scs, mud = 0.5){
-  sexchroms <- strsplit(scs, split = "")[[1]]
-  if(scs %in% c("XO", "XXO", "XY", "XYY", "XXY", "XYYY", "XXXY")){
-    sexchroms <- strsplit(scs, split="")[[1]]
-    Xs <- sum(sexchroms == "X")
-    Y <- sum(sexchroms == "Y")
-    Ds <- Da + Xs + Y
-    Dd <- Da + 2 * Xs
-    res <- mud * ((Da * (Da - 2)) / (Dd * (Dd - 2))) +
-      (1 - mud) * ((Da * (Da - 2)) / (Ds * (Ds - 2)))
-  }
-  if(scs %in% c("ZO", "ZZO", "ZW", "ZWW", "ZZW", "ZWWW", "ZZZW")){
-    Zd <- sum(sexchroms == "Z")
-    W <- sum(sexchroms == "W")
-    Dd <- Da + Zd + W
-    Ds <- Da + 2 * Zd
-    mus <- 1-mud
-    res <- mus * ((Da * (Da - 2)) / (Ds * (Ds - 2))) +
-      (1 - mus) * ((Da * (Da - 2)) / (Dd * (Dd - 2)))
-  }
-  return(res)
+pfaa <- function(Da, scs, mud = 0.5){
+  
 }
-# function for P(SS)
-Pfss <- function(Da, scs, mud = 0.5){
-  sexchroms <- strsplit(scs, split = "")[[1]]
-  if(scs %in% c("XO", "XXO", "XY", "XYY", "XXY", "XYYY", "XXXY")){
-    Xs <- sum(sexchroms == "X")
-    Y <- sum(sexchroms == "Y")
-    Ds <- Da + Xs + Y
-    Dd <- Da + 2 * Xs
-    res <- mud * ((2 * Xs * (2 * Xs - 2)) / (Dd * (Dd - 2))) +
-      (1 - mud) * ((max(Xs, Y) * (max(Xs, Y) - 1))/(Ds * (Ds - 2)))
-  }
-  if(scs %in% c("ZO", "ZZO", "ZW", "ZWW", "ZZW", "ZWWW", "ZZZW")){
-    Zd <- sum(sexchroms == "Z")
-    W <- sum(sexchroms == "W")
-    Dd <- Da + Zd + W
-    Ds <- Da + 2 * Zd
-    mus <- 1 - mud
-    res <- mus * ((2 * Zd * (2 * Zd - 2))/(Ds * (Ds - 2))) +
-      (1 - mus) * ((max(Zd, W) * (max(Zd, W) - 1))/(Dd * (Dd - 2)))
-  }
-  return(res)
-}
-
-Pfsa2 <- function (Da, Xs, Y, Dd, Da, mud = 0.5){
-  res
-}
-
-
-
-
 
 
 # calculating pval from line 194
@@ -98,3 +57,71 @@ p.val <- sum
 p.val
 
 
+
+##### Antiquated #####
+# Pfsa <- function(Da, scs, mud = 0.5){
+#   sexchroms <- strsplit(scs, split = "")[[1]]
+#   if(scs %in% c("XO", "XXO", "XY", "XYY", "XXY", "XYYY", "XXXY")){
+#     Xs <- sum(sexchroms == "X")
+#     Y <- sum(sexchroms == "Y")
+#     Ds <- Da + Xs + Y
+#     Dd <- Da + 2 * Xs
+#     res <- 1 - mud * ((Da * (Da - 2) + 2 * Xs * (2 * Xs - 2)) / (Dd * (Dd - 2))) -
+#       (1 - mud) * ((Da * (Da - 2) + max(c(Xs, Y)) * (max(c(Xs, Y)) - 1)) / (Ds * (Ds - 2)))
+#   }
+#   if(scs %in% c("ZO", "ZZO", "ZW", "ZWW", "ZZW", "ZWWW", "ZZZW")){
+#     Zd <- sum(sexchroms == "Z")
+#     W <- sum(sexchroms == "W")
+#     Dd <- Da + Zd + W
+#     Ds <- Da + 2 * Zd
+#     mus <- 1-mud
+#     res <- 1 - mus * ((Da * (Da - 2) + 2 * Zd * (2 * Zd - 2)) / (2 * Ds * (Ds - 2))) -
+#       (1 - mus) * ((Da * (Da - 2) + max(c(Zd, W)) * (max(c(Zd, W)) - 1)) / (2 * Dd * (Dd - 2)))
+#   }
+#   return(res)
+# }
+# # function for P(AA)
+# Pfaa <- function(Da, scs, mud = 0.5){
+#   sexchroms <- strsplit(scs, split = "")[[1]]
+#   if(scs %in% c("XO", "XXO", "XY", "XYY", "XXY", "XYYY", "XXXY")){
+#     sexchroms <- strsplit(scs, split="")[[1]]
+#     Xs <- sum(sexchroms == "X")
+#     Y <- sum(sexchroms == "Y")
+#     Ds <- Da + Xs + Y
+#     Dd <- Da + 2 * Xs
+#     res <- mud * ((Da * (Da - 2)) / (Dd * (Dd - 2))) +
+#       (1 - mud) * ((Da * (Da - 2)) / (Ds * (Ds - 2)))
+#   }
+#   if(scs %in% c("ZO", "ZZO", "ZW", "ZWW", "ZZW", "ZWWW", "ZZZW")){
+#     Zd <- sum(sexchroms == "Z")
+#     W <- sum(sexchroms == "W")
+#     Dd <- Da + Zd + W
+#     Ds <- Da + 2 * Zd
+#     mus <- 1-mud
+#     res <- mus * ((Da * (Da - 2)) / (Ds * (Ds - 2))) +
+#       (1 - mus) * ((Da * (Da - 2)) / (Dd * (Dd - 2)))
+#   }
+#   return(res)
+# }
+# # function for P(SS)
+# Pfss <- function(Da, scs, mud = 0.5){
+#   sexchroms <- strsplit(scs, split = "")[[1]]
+#   if(scs %in% c("XO", "XXO", "XY", "XYY", "XXY", "XYYY", "XXXY")){
+#     Xs <- sum(sexchroms == "X")
+#     Y <- sum(sexchroms == "Y")
+#     Ds <- Da + Xs + Y
+#     Dd <- Da + 2 * Xs
+#     res <- mud * ((2 * Xs * (2 * Xs - 2)) / (Dd * (Dd - 2))) +
+#       (1 - mud) * ((max(Xs, Y) * (max(Xs, Y) - 1))/(Ds * (Ds - 2)))
+#   }
+#   if(scs %in% c("ZO", "ZZO", "ZW", "ZWW", "ZZW", "ZWWW", "ZZZW")){
+#     Zd <- sum(sexchroms == "Z")
+#     W <- sum(sexchroms == "W")
+#     Dd <- Da + Zd + W
+#     Ds <- Da + 2 * Zd
+#     mus <- 1 - mud
+#     res <- mus * ((2 * Zd * (2 * Zd - 2))/(Ds * (Ds - 2))) +
+#       (1 - mus) * ((max(Zd, W) * (max(Zd, W) - 1))/(Dd * (Dd - 2)))
+#   }
+#   return(res)
+# }
---
+++ testing.R
@@ -0,0 +1,17 @@
+## XO
+Pfsa(Da = 6, scs = 'XO')==Pfsa2(6,1,0,8,7)
+
+## XXO
+Pfsa(Da = 6, scs = 'XXO')==Pfsa2(6,2,0,10,8)
+
+## XY
+Pfsa(Da = 6, scs = 'XY')==Pfsa2(6,1,1,8,8)
+
+## XYY
+Pfsa(Da = 6, scs = 'XYY')==Pfsa2(6,1,2,8,9)
+
+## XXY
+Pfsa(Da = 6, scs = 'XXY')==Pfsa2(6,2,1,10,9)
+
+## XYYY
+Pfsa(Da = 6, scs = 'XXXY')==Pfsa2(6,3,1,12,10)
